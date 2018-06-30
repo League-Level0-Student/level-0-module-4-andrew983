69b04b23d2e2eedf0356eca696e6a99df44db9c5
League <League@AdminisorsiMac2.attlocal.net>
2018-06-30 12:54:05 -0700

    ChangeCalc complete, FruitQuiz in progress

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -13,18 +13,35 @@ public class ChangeCalculator {
 
 	public static void main(String[] args) {
 
-		// Ask the user how many nickels they have
+		
+ String nickel = JOptionPane.showInputDialog("how many nickels u haves?");
 
+int a = Integer.parseInt(nickel)*5;
+
+String dime = JOptionPane.showInputDialog("what amount of dime lighjts do u hav?");
+
+int b = Integer.parseInt(dime)*10;
+
+String quarter = JOptionPane.showInputDialog("how many quarters does yOU posesss?");
+		
+int c = Integer.parseInt(quarter)*25;
+	
+double amount = a+b+c;
+
+JOptionPane.showMessageDialog(null, "You Have " +amount/100 +" dollars!");
+
+
+// Ask the user how many nickels they have
 		// Convert their answer to an int using Integer.parseInt()
+// Ask the user how many dimes they have, and convert their answer
+// Ask the user how many quarters they have, and convert their answer
 
-		// Ask the user how many dimes they have, and convert their answer
 
-		// Ask the user how many quarters they have, and convert their answer
 
-		// Calculate how much money the user has and save it in a double variable 
+
+// Calculate how much money the user has and save it in a double variable 
 
 		// Tell the user how much money they have
 
 	}
 }
-
