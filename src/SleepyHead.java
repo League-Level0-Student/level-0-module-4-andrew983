0df3cdce64e2c17fc4fff72559ec1db37dc718c7
andrew983 <league@WTS-iMac-11.attlocal.net>
2018-08-04 12:53:33 -0700

    SleepyHead, StringMethods1, StringMethods2 complete lol
    ez

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -17,28 +17,52 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-        int wer  = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Or is it a weekend?", JOptionPane.YES_NO_OPTION);
+        int wer  = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "flab", JOptionPane.YES_NO_OPTION);
         if (wer == 0) {
         isWeekday = true	;
-        	
-        	
         }
         else {
         	isWeekday = false;
         }
-        int wer  = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Or is it a weekend?", JOptionPane.YES_NO_OPTION);
-        if (wer == 0) {
-        isWeekday = true	;
-        	
-        	
+        
+    int werw  = JOptionPane.showConfirmDialog(null, "Is it a vacation??", "flab", JOptionPane.YES_NO_OPTION);
+        if (werw == 0) {
+        isVacation = true	;
         }
         else {
-        	isWeekday = false;
+        	isVacation = false;
         }
+    if(isWeekday == true) {
+     	System.out.println("get up l a  z   y    b     o      n       e        s");
+   if(isVacation == true || isWeekday == false ) { 	
+	   System.out.println("sleep in");
+   
+   }
+    }
+    if(isVacation == true) {
+    System.out.println("sleep in");	
+    }
+    
+    
+    
+  if(isWeekday == false) {
+System.out.println("sleep in");
+    	}
+}
+    	
+    }
+    
+    
+    
+   
                 /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
-         */
-    }
-}
+   */
+    
+
+
+
+    
+
--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -5,18 +5,28 @@
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+		char blah = 'v';
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String E = "Many of the goods and services we depend on daily are now supplied by intelligent, automated machines rather than human beings. Robots build cars and other goods on assembly lines, where once there were human workers. Many of our phone conversations are now conducted not with people but with sophisticated technologies. We can now buy goods at a variety of stores without the help of a human cashier. Automation is generally seen as a sign of progress, but what is lost when we replace humans with machines? Given the accelerating variety and prevalence of intelligent machines, it is worth examining the implications and meaning of their presence in our lives.\n" + 
+				"\n" + 
+				"";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(E.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+System.out.println(E.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for(int i= 0;i<E.length();i++) {
+		System.out.println(E.charAt(i));
+		if(E.charAt(i) == blah) {
+			
+			System.out.println("v is at index "+ i);
+			
+		}
+		}
+		
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
